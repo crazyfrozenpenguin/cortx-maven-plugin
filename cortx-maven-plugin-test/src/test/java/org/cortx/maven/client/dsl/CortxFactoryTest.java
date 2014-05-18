3c3f1b6a725cf3d59c2a947961fce05039549b90
Crazy Penguin <crazy.frozen.penguin@gmail.com>
2014-05-17 22:02:51 -0400

    Introduce tests for POST Verification
    Added tests that allow for the veriication of POST requests. Headers and body can be verified through the internal DSL.

--- cortx-maven-plugin-test/src/test/java/org/cortx/maven/client/dsl/CortxFactoryTest.java
+++ cortx-maven-plugin-test/src/test/java/org/cortx/maven/client/dsl/CortxFactoryTest.java
@@ -1,23 +1,24 @@
 package org.cortx.maven.client.dsl;
 
 import static org.apache.http.client.fluent.Request.Get;
+import static org.apache.http.client.fluent.Request.Post;
+import static org.apache.http.entity.ContentType.DEFAULT_TEXT;
 import static org.cortx.maven.client.CortxFactory.getCortx;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
 
-import org.apache.http.client.fluent.Request;
 import org.hamcrest.CoreMatchers;
 import org.junit.Before;
 import org.junit.Test;
 
 public class CortxFactoryTest {
 
+	private static final String CORTX_BODY = "Hello Body!";
 	private static final String TEST_VALUE = "Test Value";
 	private static final String CORTX_HEADER = "Cortx-Header";
 	private Cortx cortx;
@@ -58,6 +59,39 @@ public class CortxFactoryTest {
 		
 		// When/Then
 		assertThat("Should verify url with header was called", cortx.verify().get("/test/url").withHeader(CORTX_HEADER, TEST_VALUE).wasCalled(), is(true));
+	}
+
+	@Test(expected = UnsupportedOperationException.class)
+	public void shouldNotAllowVerifyOnGetWithBody() {
+		// When
+		cortx.verify().get("/test/url").withBody(CORTX_BODY).wasCalled();		
+	}
+	
+	@Test
+	public void shouldVerifyOnPostWasCalled() throws MalformedURLException, IOException {
+		// Given
+		Post("http://localhost:7919/test/url").execute();
+		
+		// When/Then
+		assertThat("Should verify POST to url was called", cortx.verify().post("/test/url").wasCalled(), is(true));
+	}
+	
+	@Test
+	public void shouldVerifyOnPostWithHeaderWasCalled() throws MalformedURLException, IOException {
+		// Given
+		Post("http://localhost:7919/test/url").addHeader(CORTX_HEADER, TEST_VALUE).execute();
+		
+		// When/Then
+		assertThat("Should verify POST to url with header was called", cortx.verify().post("/test/url").withHeader(CORTX_HEADER, TEST_VALUE).wasCalled(), is(true));
+	}
+
+	@Test
+	public void shouldVerifyOnPostWithBodyWasCalled() throws MalformedURLException, IOException {
+		// Given
+		Post("http://localhost:7919/test/url").bodyString(CORTX_BODY, DEFAULT_TEXT).execute();
+		
+		// When/Then
+		assertThat("Should verify POST to url with header was called", cortx.verify().post("/test/url").withHeader("Content-Type", DEFAULT_TEXT.toString()).withBody(CORTX_BODY).wasCalled(), is(true));
 	}
 
 }
